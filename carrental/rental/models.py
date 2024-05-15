09c8428bb3ff89e8a8049eae95ada91e74ca191a
Piotr Postrożny <zakopiotr353@interia.pl>
2024-05-15 13:32:34 +0200

    2024-05-08 PP

--- carrental/rental/models.py
+++ carrental/rental/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -31,10 +31,32 @@ class Car(models.Model):
     equipment = models.ManyToManyField(Equipment)
     gearbox_type = models.CharField(max_length=20, choices=ENGINE_TYPES)
     available = models.BooleanField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    value = models.DecimalField(max_digits=10, decimal_places=2)
+    
+class Address(models.Model):
+    country = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    post_code = models.CharField(max_length=10)
+    street = models.CharField(max_length=50)
+    building_no = models.CharField(max_length=10)
+    appartment_no = models.CharField(max_length=10)
+
+
+class User(User):
+    IDENTITY_DOCUMENT_TYPES = [
+        ("dowod", "dowód osobisty"),
+        ("paszport", "paszport"),
+        ("prawo_jazdy", "prawo jazdy")
+    ]
+
+    phone = models.CharField(max_length=20)
+    identity_document_type = models.CharField(max_length=20, choices=IDENTITY_DOCUMENT_TYPES)
+    identity_document_no = models.CharField(max_length=20)
+    address = models.OneToOneField(Address, on_delete=models.RESTRICT)
     
 
-class User(models.Model):
-    pass
+
 
 class Order(models.Model):
     PAYMENT_METHODS = [
