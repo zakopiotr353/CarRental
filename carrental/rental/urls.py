9b4af090e3d414c76f7dba0801d276f1aab824af
Piotr Postrożny <zakopiotr353@interia.pl>
2024-05-30 16:28:35 +0200

    2024-05-30 PP

--- carrental/rental/urls.py
+++ carrental/rental/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('car/<car_id>', views.car, name='car'),
     path('user/login', user.login, name='login'),
     path('user/logout', user.logout, name='logout'),
-    path('user/register', user.register, name='register')
+    path('user/register', user.register, name='register'),
+    path('contact/', views.contact, name='contact'),
 ]
--- carrental/rental/views.py
+++ carrental/rental/views.py
@@ -1,12 +1,19 @@
+import pandas as pd
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Car
 
-# Create your views here.
 def index(request):
-    return render(request, 'index.html.jinja')
+    return render(request,'index.html.jinja')
 
 def cars(request):
-    return render(request, 'cars.html.jinja')
+    cars = Car.objects.all()
+    categories = list(cars.values_list('category', flat=True).distinct())
+    cars2 = pd.DataFrame.from_records(cars.values())
+    return render(request,'cars.html.jinja', {'cars': cars, 'categories': categories, 'cars2': cars2.to_html(classes='table table-striped table-bordereds', index=False, table_id="cars2")})
 
 def car(request, car_id):
-    return render(request, 'car.html.jinja')
+    return render(request,'car.html.jinja')
+
+def contact(request):
+    return render(request, 'contact.html.jinja')
